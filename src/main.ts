358e075804ef75172baac446ce21f56a021c41d6
kawayiYokami <95584793+kawayiYokami@users.noreply.github.com>
2026-03-25 23:47:58 +0800

    refactor(markdown): 统一收口到 markstream-vue 渲染链

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import ConfigApp from "./apps/config/ConfigApp.vue";
 import "./style.css";
 import "cropperjs/dist/cropper.css";
 import "katex/dist/katex.min.css";
-import "@mdit/plugin-alert/style";
 import { i18n } from "./i18n";
 
 // 监听全局错误事件
